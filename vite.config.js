4c33b46208fb88237ecae14d7974904553e05b5b
dhruvjain167 <dhruvjain167@gmail.com>
2025-01-27 21:39:30 +0530

    New changes

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true
       }
     }
-  }
+  },
+  base: '/'
 });
